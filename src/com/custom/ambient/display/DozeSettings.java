3aaa4f89804c481553370e4461959b2ff8758c5c
Anushek Prasal <anushekprasal@gmail.com>
2019-12-09 19:32:15 +0530

    Revert "Allow to change default Ambient brightness values"
    This reverts commit b63b9fbcfcea87c324f4704ac52270ddbdff91e1.

--- src/com/custom/ambient/display/DozeSettings.java
+++ src/com/custom/ambient/display/DozeSettings.java
@@ -78,7 +78,6 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
         private SwitchPreference mPocketPreference;
         private SystemSettingSwitchPreference mDozeOnChargePreference;
         private SystemSettingSwitchPreference mDoubleTapPreference;
-        private Preference mBrightnessLevels;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -135,13 +134,6 @@ public class DozeSettings extends PreferenceActivity implements PreferenceFragme
                 (SwitchPreference) findPreference(Utils.GESTURE_POCKET_KEY);
             mPocketPreference.setChecked(Utils.pocketGestureEnabled(mContext));
             mPocketPreference.setOnPreferenceChangeListener(this);
-
-            mBrightnessLevels = (Preference) findPreference("doze_brightness");
-            if (mBrightnessLevels != null
-                    && !mContext.getResources().getBoolean(
-                            R.bool.hasDozeBrightnessSensor)) {
-                getPreferenceScreen().removePreference(mBrightnessLevels);
-            }
 
             mTiltCategory = (PreferenceCategory) findPreference(KEY_CATEGORY_TILT_SENSOR);
             if (!getResources().getBoolean(R.bool.has_tilt_sensor)) {
